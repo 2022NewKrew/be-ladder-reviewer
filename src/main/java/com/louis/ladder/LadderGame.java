3c949a5ac18f4548a542fb84732572dbe753b73c
louis-amu <louis.amu@kakaocorp.com>
2021-12-14 18:55:25 +0900

    기본 환경 세팅

---
+++ src/main/java/com/louis/ladder/LadderGame.java
@@ -0,0 +1,9 @@
+package com.louis.ladder;
+
+public class LadderGame {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/com/louis/ladder/LadderGameTest.java
@@ -0,0 +1,13 @@
+package com.louis.ladder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderGameTest {
+
+    @Test
+    void ladderGameTest() {
+
+    }
+}
