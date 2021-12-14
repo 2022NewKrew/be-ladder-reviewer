b4d818cbf4f0ca9336daf02ea5ba74b9a860428c
louis-amu <louis.amu@kakaocorp.com>
2021-12-14 19:51:22 +0900

    사다리 파츠 구현

--- src/main/java/com/louis/ladder/game/LadderParts.java
+++ src/main/java/com/louis/ladder/game/LadderParts.java
@@ -1,4 +1,24 @@
 package com.louis.ladder.game;
 
 public enum LadderParts {
+    VERTEX("|"),
+    MOVE_PARTS("-"),
+    BLINK(" ");
+
+    private final String parts;
+
+    LadderParts(String parts) {
+        this.parts = parts;
+    }
+
+    public static LadderParts of(int partsIndex) {
+        if (partsIndex % 2 == 0) return VERTEX;
+        return BLINK;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.parts;
+    }
 }
