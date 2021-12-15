8f79fc8c20ebe3726a3afa281f7997ec2d328c41
aiden.sim <aiden.sim@kakaocorp.com>
2021-12-15 23:00:45 +0900

    [chore] 변수명 통일

--- src/main/java/com/kakao/ladder/domain/factory/LineFactory.java
+++ src/main/java/com/kakao/ladder/domain/factory/LineFactory.java
@@ -11,24 +11,24 @@ import java.util.stream.IntStream;
 
 public class LineFactory {
     private static final int RANGE_START = 1;
-    private static final int MINIMUM_HEIGHT_SIZE = 1;
-    private static final int MINIMUM_PERSON_SIZE = 2;
+    private static final int MINIMUM_LADDER_HEIGHT = 1;
+    private static final int MINIMUM_PARTICIPANT_NUMBER = 2;
 
     private final LineStrategy lineStrategy = new RandomLineStrategy();
 
-    public List<Line> createLines(int countOfPerson, int heightOfLadder) {
-        Preconditions.checkMinimumSize(countOfPerson, MINIMUM_PERSON_SIZE,
-                                       String.format("사람 수는 %s 이상 이어야 합니다.", MINIMUM_PERSON_SIZE));
-        Preconditions.checkMinimumSize(heightOfLadder, MINIMUM_HEIGHT_SIZE,
-                                       String.format("사다리 높이는 %s 이상 이어야 합니다.", MINIMUM_HEIGHT_SIZE));
+    public List<Line> createLines(int numberOfParticipant, int heightOfLadder) {
+        Preconditions.checkMinimumSize(numberOfParticipant, MINIMUM_PARTICIPANT_NUMBER,
+                                       String.format("사람 수는 %s 이상 이어야 합니다.", MINIMUM_PARTICIPANT_NUMBER));
+        Preconditions.checkMinimumSize(heightOfLadder, MINIMUM_LADDER_HEIGHT,
+                                       String.format("사다리 높이는 %s 이상 이어야 합니다.", MINIMUM_LADDER_HEIGHT));
 
         return IntStream.rangeClosed(RANGE_START, heightOfLadder)
                 .boxed()
-                .map(h -> createLine(countOfPerson))
+                .map(h -> createLine(numberOfParticipant))
                 .collect(Collectors.toList());
     }
 
-    private Line createLine(Integer countOfPerson) {
-        return Line.of(lineStrategy.createPoints(countOfPerson));
+    private Line createLine(Integer numberOfParticipant) {
+        return Line.of(lineStrategy.createPoints(numberOfParticipant));
     }
 }
