d934d625339110343eb70a990c3a5755aaff4206
Yeon Soo Kim <skytear7@naver.com>
2021-12-15 22:06:18 +0900

    사다리게임 1단계 구현 (#10)
    * 사다리 게임 1단계 코드 작성
    
    * 2차원 배열에 사다리 틀을 넣는 형태로, 사다리가 없을수도 있는 상황 구현 추가
    
    * 메소드를 잘게 분리(메소드당 라인을 줄이기 위해)
    
    Co-authored-by: Kayle <kayle.kys@kakaocorp.com>

---
+++ src/main/java/com/kakao/LadderGame.java
@@ -0,0 +1,66 @@
+package com.kakao;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class LadderGame {
+    private int participantCount;
+    private int maxLadderHeight;
+    private String[][] ladderFrame;
+
+    public void readInput() {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("참여할 사람은 몇 명인가요?");
+        var inputParticipantCount = scanner.next();
+        this.participantCount = Integer.parseInt(inputParticipantCount);
+
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        var inputMaxLadderHeight = scanner.next();
+        this.maxLadderHeight = Integer.parseInt(inputMaxLadderHeight);
+    }
+
+    public void createLadderFrame() {
+        var ladderFrame = new String[maxLadderHeight][participantCount];
+        for (int heightIndex = 0; heightIndex < ladderFrame.length; heightIndex++) {
+            Arrays.fill(ladderFrame[heightIndex], "|");
+        }
+        this.ladderFrame = ladderFrame;
+    }
+
+    public void printLadder() {
+        var random = new Random();
+        for (int heightIndex = 0; heightIndex < ladderFrame.length; heightIndex++) {
+            // Random.nextInt는 0부터 시작(가장 높은 수가 나오면 사다리 생략)
+            var randomNumber = random.nextInt(participantCount);
+            printLine(heightIndex, randomNumber);
+        }
+    }
+
+    private void printLine(int heightIndex, int randomNumber) {
+        // 사다리 놓기
+        for (int participantIndex = 0; participantIndex < ladderFrame[heightIndex].length; participantIndex++) {
+            System.out.print(ladderFrame[heightIndex][participantIndex]);
+            // 랜덤 번호에 해당할 경우 사다리를 놓는다(사다리를 넣을 사이 공간), 마지막 자리엔 사다리를 놓지 않는다
+            if (participantIndex == randomNumber && participantIndex + 1 != ladderFrame[heightIndex].length) {
+                System.out.print("-");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println("");
+    }
+
+    public void start() {
+        readInput();
+        createLadderFrame();
+        printLadder();
+    }
+
+    public static void main(String[] args) {
+        var ladderGame = new LadderGame();
+
+        ladderGame.start();
+    }
+}
