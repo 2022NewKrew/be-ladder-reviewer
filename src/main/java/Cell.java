5f3046564afdbd0f36308b6bcb534324b0e54c07
aiden.72 <aiden.72@kakaocorp.com>
2021-12-15 16:52:45 +0900

    배열에 사다리타기 그리는 로직 추가 사다리 한칸을 int형 대신 enum으로 관리

---
+++ src/main/java/Cell.java
@@ -0,0 +1,13 @@
+public enum Cell {
+    BLANK(" "), VERTICAL("|"), HORIZONTAL("-");
+
+    private final String expression;
+
+    Cell(String expression) {
+        this.expression = expression;
+    }
+
+    public String toString() {
+        return expression;
+    }
+}
--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Ladder {
     private final Random rand = new Random();
-    private final int[][] ladder;
+    private final Cell[][] ladder;
     private final int width;
     private final int height;
     private final int numberOfPeople;
@@ -15,7 +15,48 @@ public class Ladder {
         this.numberOfPeople = numberOfPeople;
         this.width = numberOfPeople * 2 - 1;
         this.height = height;
-        this.ladder = new int[width][height];
+        this.ladder = new Cell[width][height];
+        fillLadder();
+    }
+
+    private void fillLadder() {
+        initBlank();
+        initVerticalLine();
+        initHorizontalLine();
+    }
+
+    private void initBlank() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                ladder[i][j] = Cell.BLANK;
+            }
+        }
+    }
+
+    private void initVerticalLine() {
+        for (int i = 0; i < numberOfPeople; i++) {
+            for (int j = 0; j < height; j++) {
+                ladder[i * 2][j] = Cell.VERTICAL;
+            }
+        }
+    }
+
+    public void initHorizontalLine() {
+        for (int i = 0; i < (width - numberOfPeople); i++) {
+            for (int j = 0; j < height; j++) {
+                if (rand.nextBoolean())
+                    ladder[i * 2 + 1][j] = Cell.HORIZONTAL;
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                System.out.print(ladder[j][i].toString());
+            }
+            System.out.println();
+        }
     }
 
     public void validateInput(int numberOfPeople, int height) {
