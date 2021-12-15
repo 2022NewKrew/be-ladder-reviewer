f918050da764a9b0e82ccf6bf8d97bc3f1c02e1b
june.kim <junho85@gmail.com>
2021-12-16 01:19:54 +0900

    feat: STEP1 간단한 사다리 게임 구현
    프로젝트 초기 구성과 간단한 사다리 게임 구현
    
    프로젝트 초기 구성
    * 무난하게 요즘 많이 사용하는 gradle을 사용
    
    기능 요구사항
    * n명의 사람과 m개의 사다리 개수 입력
    * 사다리의 라인(rung)은 랜덤 값에 따라 있거나 없다
    * 사다리가 있으면 "-", 없으면 " "(빈공백). 양옆에는 "|"로 세로를 표시
    * 사다리 상태를 화면에 출력. 어느 시점에 출력할 것인지에 대한 제약은 없음
    
    프로그램 요구사항
    * 메서드의 크기가 최대 10라인을 넘지 않도록 구현
    ** method가 한 가지 일만 하도록 최대한 작게 만들기

---
+++ src/main/java/com/kakao/june/LadderGameApplication.java
@@ -0,0 +1,8 @@
+package com.kakao.june;
+
+public class LadderGameApplication {
+    public static void main(String[] args) {
+        LadderGame ladderGame = new LadderGame();
+        ladderGame.run();
+    }
+}
