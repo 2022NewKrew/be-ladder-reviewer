b921f40b67daebd8f40fa30d66f4d62198a8748b
eli nabro <eli.nabro@kakaocorp.com>
2021-12-15 22:28:55 +0900

    feat : width 추가

--- app/src/main/java/ladder/domain/Ladder.java
+++ app/src/main/java/ladder/domain/Ladder.java
@@ -3,13 +3,19 @@ package ladder.domain;
 public class Ladder {
 
     private final int height;
+    private final int width;
 
-    public Ladder(int height) {
+    public Ladder(int height, int width) {
         this.height = height;
+        this.width = width;
     }
 
     public int getHeight() {
         return height;
+    }
+
+    public int getWidth() {
+        return width;
     }
 
 }
--- app/src/test/java/ladder/domain/LadderTest.java
+++ app/src/test/java/ladder/domain/LadderTest.java
@@ -7,12 +7,22 @@ public class LadderTest {
 
     @Test
     void heightTest() {
-        Ladder ladder1 = new Ladder(3);
-        Ladder ladder2 = new Ladder(4);
-        Ladder ladder3 = new Ladder(5);
+        Ladder ladder1 = new Ladder(3, 5);
+        Ladder ladder2 = new Ladder(4, 2);
+        Ladder ladder3 = new Ladder(5, 3);
         assertThat(ladder1.getHeight()).isEqualTo(3);
         assertThat(ladder2.getHeight()).isEqualTo(4);
         assertThat(ladder3.getHeight()).isEqualTo(5);
+    }
+
+    @Test
+    void widthTest() {
+        Ladder ladder1 = new Ladder(3, 5);
+        Ladder ladder2 = new Ladder(4, 7);
+        Ladder ladder3 = new Ladder(5, 4);
+        assertThat(ladder1.getWidth()).isEqualTo(5);
+        assertThat(ladder2.getWidth()).isEqualTo(7);
+        assertThat(ladder3.getWidth()).isEqualTo(4);
     }
 
 }
