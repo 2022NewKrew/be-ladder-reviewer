e8aaf39d3656e593d638f9b86cd009482599b938
KimMinjeong <mor2222@naver.com>
2021-12-15 22:06:16 +0900

    사다리 게임 1단계 구현 (#8)
    * init: first commit
    
    * feat: 사다리게임 기능개발
    
    Co-authored-by: jyami.kim <jyami.kim@kakaocorp.com>

---
+++ src/test/java/com/kakao/ladder/domain/LadderLineTest.java
@@ -0,0 +1,17 @@
+package com.kakao.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderLineTest {
+    @Test
+    @DisplayName("사람 수를 입력하여, 사다리의 가로줄 하나를(height=1) 생성한다.")
+    public void init() {
+        int sizeOfPerson = 5;
+        LadderLine ladderLine = LadderLine.init(sizeOfPerson);
+        assertThat(ladderLine.getPoints().size()).isEqualTo(sizeOfPerson);
+    }
+
+}
