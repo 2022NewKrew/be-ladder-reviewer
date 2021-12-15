2e548eb8e52bc3ac587acfd09685d25361bb0c53
aiden.72 <aiden.72@kakaocorp.com>
2021-12-15 18:52:55 +0900

    10라인 넘어가는 메소드 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,10 @@ public class Main {
         int numberOfPeople = scanner.nextInt();
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         int heightOfLadder = scanner.nextInt();
+        createLadder(numberOfPeople, heightOfLadder);
+    }
+
+    private static void createLadder(int numberOfPeople, int heightOfLadder) {
         try {
             Ladder ladder = new Ladder(numberOfPeople, heightOfLadder);
             ladder.print();
