7b43e06d04dd2229f693767eeaf9fe8820f25b23
심준보 <s860620@gmail.com>
2021-12-16 04:01:23 +0900

    사다리 게임 2단계 구현 (#23)
    * [feature] gradle 초기 환경 구성
    
    * [feature] 사다리 한줄에 대한 Line 도메인 추가
    
    * [feature] Line 생성 Factory와 사다리에 대한 Ladder 도메인 추가
    
    * [feature] 필수값 체크 유틸 추가
    
    * [feature] 사다리 유스케이스 추가 (생성)
    
    * [feature] 세부사항들 구현 (웹, 콘솔)
    
    * [docs] 기능 문서 정리
    
    * gitignore 다시 적용
    
    * [feature] 세부사항들 구현 (웹, 콘솔)
    
    * remove intellihj .idea directory
    
    * remove intellihj gradle directory
    
    * gitignore 다시 적용
    
    * [docs] 문서 수정
    
    * [refactor] 라인생성에 대해서 전략패턴으로 변경
    
    * [chore] 오타, 포멧 변경
    
    * [chore] 변수명 통일
    
    * [refactor] for문 하나의 형태로 통일
    
    * [chore] 변수명 통일

--- src/main/java/com/kakao/ladder/domain/factory/LineFactory.java
+++ src/main/java/com/kakao/ladder/domain/factory/LineFactory.java
@@ -1,51 +1,34 @@
 package com.kakao.ladder.domain.factory;
 
-import com.kakao.ladder.domain.Line;
 import com.kakao.ladder.common.Preconditions;
+import com.kakao.ladder.domain.Line;
+import com.kakao.ladder.domain.strategy.LineStrategy;
+import com.kakao.ladder.domain.strategy.RandomLineStrategy;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LineFactory {
-    private static final Random RANDOM = new Random();
-    private static final int START_POINT_INDEX = 1;
-    private static final int PREVIOUS_POINT_INDEX = -1;
-
     private static final int RANGE_START = 1;
-    private static final int MINIMUM_HEIGHT_SIZE = 1;
-    private static final int MINIMUM_PERSON_SIZE = 2;
+    private static final int MINIMUM_LADDER_HEIGHT = 1;
+    private static final int MINIMUM_PARTICIPANT_NUMBER = 2;
 
-    public List<Line> createLines(int countOfPerson, int heightOfLadder) {
-        Preconditions.checkMinimumSize(countOfPerson, MINIMUM_PERSON_SIZE,
-                                       String.format("사람 수는 %s 이상 이어야 합니다.", MINIMUM_PERSON_SIZE));
-        Preconditions.checkMinimumSize(heightOfLadder, MINIMUM_HEIGHT_SIZE,
-                                       String.format("사다리 높이는 %s 이상 이어야 합니다.", MINIMUM_HEIGHT_SIZE));
+    private final LineStrategy lineStrategy = new RandomLineStrategy();
+
+    public List<Line> createLines(int numberOfParticipant, int heightOfLadder) {
+        Preconditions.checkMinimumSize(numberOfParticipant, MINIMUM_PARTICIPANT_NUMBER,
+                                       String.format("사람 수는 %s 이상 이어야 합니다.", MINIMUM_PARTICIPANT_NUMBER));
+        Preconditions.checkMinimumSize(heightOfLadder, MINIMUM_LADDER_HEIGHT,
+                                       String.format("사다리 높이는 %s 이상 이어야 합니다.", MINIMUM_LADDER_HEIGHT));
 
         return IntStream.rangeClosed(RANGE_START, heightOfLadder)
                 .boxed()
-                .map(h -> Line.of(createPoints(countOfPerson)))
+                .map(h -> createLine(numberOfParticipant))
                 .collect(Collectors.toList());
     }
 
-    private List<Boolean> createPoints(int countOfPerson) {
-        List<Boolean> points = new ArrayList<>(Collections.nCopies(countOfPerson, false));
-
-        IntStream.range(START_POINT_INDEX, countOfPerson)
-                .forEach(index -> {
-                    boolean previousPoint = points.get(index + PREVIOUS_POINT_INDEX);
-                    points.set(index, nextPoint(previousPoint));
-                });
-        return points;
-    }
-
-    private boolean nextPoint(boolean previousPoint) {
-        if (previousPoint) {
-            return false;
-        }
-        return RANDOM.nextBoolean();
+    private Line createLine(Integer numberOfParticipant) {
+        return Line.of(lineStrategy.createPoints(numberOfParticipant));
     }
 }
