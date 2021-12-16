4ab3b953e16b0fdefe9395b83697928c23e48086
eli nabro <eli.nabro@kakaocorp.com>
2021-12-16 21:36:51 +0900

    feat : UI 예외처리 추가.
    숫자 외 텍스트 입력시 0으로 반환

--- app/src/main/java/ladder/view/LadderGameUI.java
+++ app/src/main/java/ladder/view/LadderGameUI.java
@@ -10,12 +10,26 @@ public class LadderGameUI {
 
     public static int inputPeopleSize() {
         System.out.println(HOW_MANY_PEOPLE);
-        return Integer.parseInt(scanner.nextLine());
+        int numOfPeople;
+        try {
+            String input = scanner.nextLine();
+            numOfPeople = Integer.parseInt(input);
+        } catch (Exception e) {
+            numOfPeople = 0;
+        }
+        return numOfPeople;
     }
 
     public static int inputLadderHigh() {
         System.out.println(HOW_HIGH);
-        return Integer.parseInt(scanner.nextLine());
+        int highOfLadder;
+        try {
+            String input = scanner.nextLine();
+            highOfLadder = Integer.parseInt(input);
+        } catch (Exception e) {
+            highOfLadder = 0;
+        }
+        return highOfLadder;
     }
 
     public static void outputLadder(String ladder) {
