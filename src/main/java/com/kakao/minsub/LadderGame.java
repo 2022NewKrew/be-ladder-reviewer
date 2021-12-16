3be002714924e3dc8e20f33249a5bb88da303e76
gray.ji <gray.ji@kakaocorp.com>
2021-12-16 09:57:10 +0900

    step2

--- src/main/java/com/kakao/minsub/LadderGame.java
+++ src/main/java/com/kakao/minsub/LadderGame.java
@@ -1,11 +1,6 @@
 package com.kakao.minsub;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LadderGame {
     
@@ -16,27 +11,7 @@ public class LadderGame {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         int ladderHeight = scanner.nextInt();
         
-        printLadder(generateLadder(playerCount, ladderHeight));
-    }
-    
-    private static List<List<Boolean>> generateLadder(int playerCount, int height) {
-        final Random random = new Random();
-        return IntStream.range(0, height).mapToObj(i -> {
-            List<Boolean> lines = new ArrayList<>();
-            boolean preLine = false;
-            for (int j=0; j<playerCount-1; j++) {
-                boolean line = preLine ? false : random.nextBoolean();
-                lines.add(line);
-                preLine = line;
-            }
-            return lines;
-        }).collect(Collectors.toList());
-    }
-    
-    private static void printLadder(List<List<Boolean>> ladder) {
-        ladder.forEach(line -> {
-            line.forEach(e -> System.out.print("|" + (e ? "--" : "  ")));
-            System.out.println("|  ");
-        });
+        Ladder ladder = new Ladder(playerCount, ladderHeight);
+        ladder.print();
     }
 }
