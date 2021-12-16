3575a5a582cc1c00d4345b388b66310f51206736
DH Kim <inerplat@gmail.com>
2021-12-16 22:06:30 +0900

    Step1: 사다리게임 1단계 구현 (#50)
    * [step1] 사다리게임 기본 구현
    
    * [step1] Ladder class Public 제거
    
    * [step1] column 수 조정
    
    Co-authored-by: robin.king <robin.king@kakaocorp.com>

---
+++ src/Ladder.java
@@ -0,0 +1,48 @@
+class Ladder {
+    private final int row;
+    private final int col;
+    private final boolean[][] ladderArr;
+
+    public Ladder(int row, int col) {
+        this.row = row;
+        this.col = col - 1;
+        if (row < 0 || col < 0) {
+            throw new IllegalArgumentException("row and col must be positive");
+        }
+        ladderArr = new boolean[row][col];
+    }
+
+    private boolean isRange(int r, int c) {
+        return (0 <= r && r < row) && (0 <= c && c < col);
+    }
+
+    private boolean isBlank(int r, int c) {
+        return !ladderArr[r][c];
+    }
+
+    private boolean isValid(int r, int c) {
+        final boolean leftOk = !isRange(r, c - 1) || isBlank(r, c - 1);
+        final boolean rightOk = !isRange(r, c + 1) || isBlank(r, c + 1);
+        return isRange(r, c) && leftOk && rightOk;
+    }
+
+    public void createLineByRandom() {
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (isValid(r, c) && Math.random() < 0.5) {
+                    ladderArr[r][c] = true;
+                }
+            }
+        }
+    }
+
+    public void printLadder() {
+        for (int i = 0; i < row; i++) {
+            System.out.print("|");
+            for (int j = 0; j < col; j++) {
+                System.out.print(ladderArr[i][j] ? "-|" : " |");
+            }
+            System.out.println();
+        }
+    }
+}
