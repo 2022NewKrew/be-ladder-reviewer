aafbab1e7648c67ce2e3747a55073498faea0688
zero.main <zero.main@kakaocorp.com>
2021-12-16 16:51:30 +0900

    step2

--- ladder-reviewer/src/main/java/kakao/reviewer/sample/LadderBuilder.java
+++ ladder-reviewer/src/main/java/kakao/reviewer/sample/LadderBuilder.java
@@ -6,6 +6,7 @@ public class LadderBuilder {
 
     private final int width;
     private final int height;
+    private static final Random rand = new Random();
 
     public LadderBuilder(int width, int height) {
         this.width = width;
@@ -13,33 +14,41 @@ public class LadderBuilder {
     }
 
     public boolean[][] buildLadder() {
-        Random rand = new Random();
         boolean[][] ladder = new boolean[height][width];
 
         for(int i = 0; i < height; i++) {
-            for(int j = 0; j < width - 1; j++) {
-                ladder[i][j] = rand.nextBoolean();
-            }
+            buildRandomBridgeForStep(ladder, i);
         }
         return ladder;
     }
 
+    private void buildRandomBridgeForStep(boolean[][] ladder, int step) {
+        for(int j = 0; j < width - 1; j++) {
+            ladder[step][j] = rand.nextBoolean();
+        }
+    }
+
     public void printLadder(boolean[][] ladder) {
         for(int i = 0; i < height; i++) {
-            for(int j = 0; j < width; j++) {
-                System.out.print("|");
-                printBridge(ladder[i][j]);
-            }
+            printLadderStep(ladder, i);
             System.out.println();
         }
     }
 
-    private void printBridge(boolean isBridge) {
-        if(isBridge) {
-            System.out.print("-");
-        } else {
-            System.out.print(" ");
+    private void printLadderStep(boolean[][] ladder, int step) {
+        for(int j = 0; j < width; j++) {
+            System.out.print("|");
+
+            String bridge = getBridge(ladder[step][j]);
+            System.out.print(bridge);
         }
+    }
+
+    private String getBridge(boolean isBridge) {
+        if(isBridge) {
+            return "-";
+        }
+        return " ";
     }
 
 }
--- ladder-reviewer/src/main/java/kakao/reviewer/sample/Main.java
+++ ladder-reviewer/src/main/java/kakao/reviewer/sample/Main.java
@@ -1,39 +1,22 @@
 package kakao.reviewer.sample;
 
-import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) {
         try {
-            int[] input = takeInput();
+            InputReader inputReader = new InputReader();
+            int numOfPeople = inputReader.takeInput("참여할 사람은 몇 명인가요?");
+            int ladderHeight = inputReader.takeInput("최대 사다리 높이는 몇 개인가요?");
+            inputReader.closeScanner();
 
-            LadderBuilder ladderBuilder = new LadderBuilder(input[0], input[1]);
-            boolean[][] ladder = ladderBuilder.buildLadder();
-            ladderBuilder.printLadder(ladder);
+            buildLadder(numOfPeople, ladderHeight);
         } catch (Exception e) {
-            System.out.println("[FAILED] 잘못된 입력입니다: " + e.getMessage());
+            System.out.println("[FAILED] " + e.getMessage());
         }
     }
 
-    public static int[] takeInput() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("참여할 사람은 몇 명인가요?");
-        int numOfPeople = Integer.parseInt(scanner.nextLine());
-        validateInput(numOfPeople);
-
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int ladderHeight = Integer.parseInt(scanner.nextLine());
-        validateInput(ladderHeight);
-
-        scanner.close();
-        return new int[] {numOfPeople, ladderHeight};
-    }
-
-    public static void validateInput(int input) {
-        if(input <= 0) {
-            throw new RuntimeException("1 이상의 정수만 입력가능합니다.");
-        }
+    public static void buildLadder(int numOfPeople, int ladderHeight) {
+        LadderBuilder ladderBuilder = new LadderBuilder(numOfPeople, ladderHeight);
+        boolean[][] ladder = ladderBuilder.buildLadder();
+        ladderBuilder.printLadder(ladder);
     }
 }
