2e5dfc764965361741b59aa9f79e73a24a93e333
cole(박보국) <cole.c@kakaocorp.com>
2021-12-16 16:47:32 +0900

    feat: Implement LadderGame step 1

---
+++ src/test/java/cole/kakao/com/LadderGameTest.java
@@ -0,0 +1,44 @@
+package cole.kakao.com;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class LadderGameTest {
+
+    @Test
+    void testInputNumParticipants_whenNumParticipants3_then3() {
+
+        // given
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("3\n5".getBytes());
+        System.setIn(inputStream);
+
+        LadderGame ladderGame = new LadderGameCliImpl();
+
+        // when
+        LadderGame.LadderGameParams actual = ladderGame.inputGameParams(inputStream);
+
+        // then
+        assertEquals(3, actual.numParticipants);
+    }
+
+    @Test
+    void testInputMaxLadderHeight_whenMaxLadderHeight3_then3() {
+
+        // given
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("1\n3".getBytes());
+        System.setIn(inputStream);
+
+        LadderGame ladderGame = new LadderGameCliImpl();
+
+        // when
+        LadderGame.LadderGameParams actual = ladderGame.inputGameParams(inputStream);
+
+        // then
+        assertEquals(3, actual.maxLadderHeight);
+    }
+
+}
