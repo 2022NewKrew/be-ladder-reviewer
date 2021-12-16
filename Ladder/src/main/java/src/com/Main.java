9eebdd19e984e8f81585da827caade891f187c4d
shaheen-m <96107161+shaheen-m@users.noreply.github.com>
2021-12-16 22:06:24 +0900

    [사다리게임] Step1 완료 (#42)
    * [사다리게임] Step1
    
    * [사다리게임] Step1
    
    Co-authored-by: shaheen <shaheen.park@kakaocorp.com>

---
+++ Ladder/src/main/java/src/com/Main.java
@@ -0,0 +1,47 @@
+package src.com;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    static final String INSERT_USER_COMMENT = "참여할 사람은 몇 명인가요?";
+    static final String INSERT_LADDER_COMMENT = "최대 사다리 높이는 몇 개 인가요?";
+    static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        printLadder(playLadder(insertData("P"), insertData("L")));
+    }
+
+    public static int insertData(String type){
+        String coment = "P".equals(type) ? INSERT_USER_COMMENT : INSERT_LADDER_COMMENT;
+        System.out.println(coment);
+        return Integer.parseInt(sc.nextLine());
+    }
+
+    public static List<List<String>> playLadder(int persons, int height){
+        Random random1 = new Random();
+        List<List<String>> laderArray =  new ArrayList<List<String>>();
+        for(int i=0; i < height; i++){
+            List<String> lineStrArray = new ArrayList<String>();
+            for(int r=0; r< persons; r++){
+                String connect = r < persons -1 && random1.nextBoolean() ? "-" : " ";
+                lineStrArray.add("|" + connect);
+            }
+            laderArray.add(lineStrArray);
+        }
+        return laderArray;
+    }
+
+    public static void printLadder(List<List<String>> laderArray){
+        laderArray.forEach(item -> {
+            item.forEach(System.out::print);
+            System.out.println();
+        });
+    }
+
+
+
+}
