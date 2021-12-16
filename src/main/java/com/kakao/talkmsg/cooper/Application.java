f37c27234bc943f6800ce39157db821395b70fa4
cooper.lee <cooper.lee@kakaocorp.com>
2021-12-16 19:51:38 +0900

    Scanner 사용하도록 변경

--- src/main/java/com/kakao/talkmsg/cooper/Application.java
+++ src/main/java/com/kakao/talkmsg/cooper/Application.java
@@ -1,20 +1,18 @@
 package com.kakao.talkmsg.cooper;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("참여할 사람은 몇 명인가요?");
-            Integer numOfPeople = Integer.valueOf(bufferedReader.readLine());
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("최대 사다리 높이는 몇 개인가요?");
-            Integer height = Integer.valueOf(bufferedReader.readLine());
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int numOfPeople = scanner.nextInt();
 
-            Ladder ladder = new Ladder(numOfPeople, height);
-            Output.printLadder(ladder);
-        }
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int height = scanner.nextInt();
+
+        Ladder ladder = new Ladder(numOfPeople, height);
+        Output.printLadder(ladder);
     }
 }
