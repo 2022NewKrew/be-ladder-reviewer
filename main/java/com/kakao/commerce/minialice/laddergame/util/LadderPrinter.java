37aab9458af0d7691db39eabbee47949e92f735b
kero.be <kero.be@kakaocorp.com>
2021-12-16 09:31:16 +0900

    [step1] print 메소드 분리

--- main/java/com/kakao/commerce/minialice/laddergame/util/LadderPrinter.java
+++ main/java/com/kakao/commerce/minialice/laddergame/util/LadderPrinter.java
@@ -9,19 +9,25 @@ public class LadderPrinter {
     private static final String SHAFT = "|";
     private static final String FOOTBOARD = "-";
     private static final String EMPTY_FOOTBOARD = " ";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
 
     public static void print(Ladder ladder) {
+        StringBuilder ladderTextBuilder = new StringBuilder();
         for (LadderBody ladderBody : ladder.getLadderBodies()) {
-            System.out.print(SHAFT);
-            for (LadderFootboard footboard : ladderBody.getLadderFootboards()) {
-                if (footboard.isUse()) {
-                    System.out.print(FOOTBOARD);
-                } else {
-                    System.out.print(EMPTY_FOOTBOARD);
-                }
-                System.out.print(SHAFT);
+            ladderTextBuilder.append(SHAFT);
+            addFootboardText(ladderBody, ladderTextBuilder);
+            ladderTextBuilder.append(LINE_SEPARATOR);
+        }
+        System.out.println(ladderTextBuilder);
+    }
+
+    private static void addFootboardText(LadderBody ladderBody, StringBuilder ladderTextBuilder) {
+        for (LadderFootboard footboard : ladderBody.getLadderFootboards()) {
+            if (footboard.isUse()) {
+                ladderTextBuilder.append(FOOTBOARD).append(SHAFT);
+            } else {
+                ladderTextBuilder.append(EMPTY_FOOTBOARD).append(SHAFT);
             }
-            System.out.println();
         }
     }
 }
