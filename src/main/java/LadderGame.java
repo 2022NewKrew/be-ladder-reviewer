3659c92365f5c3b8eac9d934c7f826457e0ae8cb
unsouled <unsouled0@gmail.com>
2021-12-16 14:04:08 +0900

    fix syntax error

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -13,6 +13,7 @@ public class LadderGame {
 	private int askPlayerNumber(Scanner scanner) {
 		System.out.println("참여할 사람은 몇 명인가요?");
 		return scanner.nextInt();
+	}
 
 	private int askHeight(Scanner scanner) {
 		System.out.println("최대 사다리 높이는 몇 개인가요?");
