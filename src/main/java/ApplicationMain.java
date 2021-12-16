27a6a5c823fd02f0cf8e277fb23c0084e2b841a5
rayfault <rayfault@gmail.com>
2021-12-16 11:06:10 +0900

    implement readRequiredInputs method

--- src/main/java/ApplicationMain.java
+++ src/main/java/ApplicationMain.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class ApplicationMain {
     public static void main(String[] args) {
         var ladderGame = new LadderGame();
--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class LadderGame {
+
+    private int participants;
+    private int heightOfLadder;
 
     public void start() {
         readRequiredInputs();
         printLadders();
     }
 
-    private void printLadders() {
+    private void readRequiredInputs() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("참여할 사람은 몇 명인가요?");
+        this.participants = scanner.nextInt();
+
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        this.heightOfLadder = scanner.nextInt();
     }
 
-    private void readRequiredInputs() {
+    private void printLadders() {
     }
 }
