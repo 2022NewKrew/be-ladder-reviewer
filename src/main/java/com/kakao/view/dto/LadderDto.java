3f25012bae827398a83ab40bd26f1c2ee0167a68
corby <corby.kim@kakaocorp.com>
2021-12-16 20:49:11 +0900

    여러 개의 cross를 갖도록 변경

--- src/main/java/com/kakao/view/dto/LadderDto.java
+++ src/main/java/com/kakao/view/dto/LadderDto.java
@@ -5,38 +5,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.kakao.domain.Ladder;
+import com.kakao.domain.LadderRow;
 
 public class LadderDto {
 
-    private final int headCount;
-    private final int height;
-    private final List<Integer> ladderIndex;
+    private final List<List<Boolean>> isCross;
 
     public static LadderDto of(Ladder ladder) {
-        List<Integer> ladderIndexes = getLadderIndexes(ladder);
-        return new LadderDto(ladder.getHeadCount().getValue(), ladder.getHeight().getValue(), ladderIndexes);
+        List<List<Boolean>> isCross = ladder.getLadder()
+              .stream()
+              .map(LadderRow::getCrosses)
+              .collect(Collectors.toList());
+        return new LadderDto(isCross);
     }
 
-    private static List<Integer> getLadderIndexes(Ladder ladder) {
-        return ladder.getLadderCrosses()
-                     .getAllLeft();
+    public LadderDto(List<List<Boolean>> isCross) {
+        this.isCross = Collections.unmodifiableList(isCross);
     }
 
-    public LadderDto(int headCount, int height, List<Integer> ladderIndex) {
-        this.headCount = headCount;
-        this.height = height;
-        this.ladderIndex = Collections.unmodifiableList(ladderIndex);
-    }
-
-    public int getHeadCount() {
-        return headCount;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public List<Integer> getLadderIndex() {
-        return ladderIndex;
+    public List<List<Boolean>> getIsCross() {
+        return isCross;
     }
 }
