cdb51bcd3e17ac4a167a6a3262734ab5cb74b98c
nio <nio.d@kakaocorp.com>
2021-12-19 15:41:34 +0900

    refactor: 사용하지 않는 코드 정리

--- src/main/java/com/kakao/nio/ladder/UserInput.java
+++ src/main/java/com/kakao/nio/ladder/UserInput.java
@@ -10,22 +10,10 @@ public class UserInput {
         this.scanner = new Scanner(System.in);
     }
 
-    public String readLadderMemberNames() {
-        return getStringFromStdin("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-    }
-
     public int readNumOfMembers() { return getNumberFromStdin("참여할 사람은 몇 명인가요?"); }
-
-    public String readLadderResults() {
-        return getStringFromStdin("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
-    }
 
     public int readLadderHeight() {
         return getNumberFromStdin("최대 사다리 높이는 몇 개인가요?");
-    }
-
-    public String readMemberNameForResult() {
-        return getStringFromStdin("결과를 보고 싶은 사람은?");
     }
 
     private String getStringFromStdin(String displayText) {
@@ -36,12 +24,6 @@ public class UserInput {
     private int getNumberFromStdin(String displayText) {
         System.out.println(displayText);
         return Integer.valueOf(scanner.nextLine());
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        this.scanner.close();
-        super.finalize();
     }
 
 }
